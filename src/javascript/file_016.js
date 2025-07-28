76a97b210d7058356e79a96a270a4cc3eea83161
aanuoluwa-zilo <168453615+aanuoluwa-zilo@users.noreply.github.com>
2025-07-28 12:45:47 +0100

    Add concurrent test file: src/javascript/file_016.js

---
+++ src/javascript/file_016.js
@@ -0,0 +1,12 @@
+// File 16
+console.log('Hello from file 16');
+
+class TestClass16 {
+    constructor() {
+        this.value = 16;
+    }
+
+    getValue() {
+        return this.value;
+    }
+}
