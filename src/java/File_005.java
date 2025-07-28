3566c41b3b7798f037e9d90dcb9fa1987e499662
aanuoluwa-zilo <168453615+aanuoluwa-zilo@users.noreply.github.com>
2025-07-28 12:46:02 +0100

    Add concurrent test file: src/java/File_005.java

---
+++ src/java/File_005.java
@@ -0,0 +1,12 @@
+// File 5
+public class File005 {
+    private int value = 5;
+
+    public int getValue() {
+        return value;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello from file 5");
+    }
+}
